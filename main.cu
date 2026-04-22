8ec2cef35698703907288d71e4fe81808441da3b
Fayaaz Ahmed <fayaaz.ahmed.20@gmail.com>
2026-04-22 18:13:12 -0400

    Count prime numbers

--- main.cu
+++ main.cu
@@ -43,6 +43,15 @@ __global__ void sieveKernel(bool* is_prime, unsigned long long limit, unsigned l
     }    
 }
 
+__global__ void getPrimeCount(bool* is_prime, unsigned long long limit, int* count) {
+    unsigned long long idx = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned long long offset = blockDim.x * gridDim.x;
+
+    for (unsigned long long i = idx; i < limit; i += offset) {
+        if (is_prime[i]) atomicAdd(count, 1);
+    }
+}
+
 int main() {
     unsigned long long limit = 1000000000;
     unsigned long long limit_sqrt = sqrt(limit);
@@ -63,10 +72,13 @@ int main() {
     }
 
     // Check initialized prime values (0 or 1) and return total count
-    CUDA_CHECK(cudaMemcpy(h_is_prime, d_is_prime, (limit + 1) * sizeof(bool), cudaMemcpyDeviceToHost));
-    int count = 0;
-    for (int i = 0; i < limit + 1; i++) {
-        if (h_is_prime[i] == 1) count++;
-    }
-    printf("Number of primes: %d\n", count);
+    int* h_count = (int*)malloc(sizeof(int));
+    int* d_count;
+    CUDA_CHECK(cudaMalloc(&d_count, sizeof(int)));
+    CUDA_CHECK(cudaMemset(d_count, 0, sizeof(int)));
+    getPrimeCount<<<10, 10>>>(d_is_prime, limit, d_count);
+    CUDA_CHECK(cudaDeviceSynchronize());
+    CUDA_CHECK(cudaMemcpy(h_count, d_count, sizeof(int), cudaMemcpyDeviceToHost));
+    CUDA_CHECK(cudaDeviceSynchronize());
+    printf("Number of primes: %d\n", *h_count);
 }
